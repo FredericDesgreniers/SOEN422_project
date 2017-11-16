627366671857738d3569126d7be97e231a68e720
Christopher McArthur <prince.chrismc@gmail.com>
2017-11-16 15:24:56 -0500

    adding basic vcxproj to test HX711

---
+++ Arduino/HX7/HX7.ino
@@ -0,0 +1,15 @@
+/*
+    Name:      HX7.ino
+    Created:   11/16/2017 3:17:57 PM
+    Author:    cmcarthur
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+}
