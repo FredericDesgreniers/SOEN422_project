d3c1cadfff351eaaa2a4a861a605373cdedfc658
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2017-11-16 18:08:32 -0500

    Hello world website

---
+++ Website/src/main/java/me/frde/ForceWebsite/ForceWebsiteApplication.java
@@ -0,0 +1,12 @@
+package me.frde.ForceWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForceWebsiteApplication.class, args);
+	}
+}
